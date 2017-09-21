6f0e68e1ff383519e106db3d704c2e3005c83982
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-09-21 12:14:42 -0500

    Working with Node.js
    -Working with the basics of fs and os systems in node.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+console.log('Initilized app.js'); // Simply for learning to track how files are executed.
+const fs = require('fs'); // Getting the fs functions from node.js (const is used to avoid overwritting)
+const os = require('os'); // Getting os functions
+
+
+var user = os.userInfo();
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
