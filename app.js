f53741955149fd0b409c7c52935c91f5eb3317da
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-09-21 13:05:31 -0500

    Making the classic note app.
    -Adding notes.js to handle functions for notes app

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-console.log('Initilized app.js'); // Simply for learning to track how files are executed.
+console.log('Initilized app.js'); // Simply for learning to track how and when files are executed.
 const fs = require('fs'); // Getting the fs functions from node.js (const is used to avoid overwritting)
 const os = require('os'); // Getting os functions
+const notes = require('./notes.js');
 
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+var add = notes.add;
+console.log(add(1, 5));
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
---
+++ notes.js
@@ -0,0 +1,9 @@
+console.log('Initilized notes.js');
+module.exports.addNote = function () {
+    console.log('addNote');
+    return "New Note";
+};
+
+module.exports.add = function (x, y) {
+    return x + y;
+};
