ad03cada5593d649fe6564527f7c6d33788aafb5
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-09-22 15:06:17 -0500

    Lodash
    -Now one can preform NPM install to get the Dependencies.

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 console.log('Initilized app.js'); // Simply for learning to track how and when files are executed.
+
+//Dependencies
 const fs = require('fs'); // Getting the fs functions from node.js (const is used to avoid overwritting)
 const os = require('os'); // Getting os functions
+const _ = require('lodash');
+
+// My Files
 const notes = require('./notes.js');
 
+var filteredArray = _.uniq(['Scot', 'Scot', 1, 1, 2, 3, 4, 5, 6, 7]);
+console.log(filteredArray);
 
-var res = notes.addNote();
-console.log(res);
+console.log(_.isString('Scot'));
+// var res = notes.addNote();
+// console.log(res);
 
-var add = notes.add;
-console.log(add(1, 5));
+// var add = notes.add;
+// console.log(add(1, 5));
 // var user = os.userInfo();
 
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
