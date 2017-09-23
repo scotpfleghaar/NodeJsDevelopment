8e3c6e18576fee6dc4d0a81400d60aa867930a6a
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-09-23 13:21:58 -0500

    Working with Argv to campture input
    -Rather than using the build it argv command, I am going to use a package to better capture input

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const _ = require('lodash');
 
 // My Files
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log(process.argv);
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('Adding New Note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing Note');
+} else {
+    console.log('Command not recongized');
+}
